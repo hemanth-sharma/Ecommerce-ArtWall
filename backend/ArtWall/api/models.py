158dddb05809015629aefa0882237511136396dd
Hemant Sharma <hemantsharmafli7@gmail.com>
2024-10-15 23:37:39 +0530

    Updated backend category model

--- backend/ArtWall/api/models.py
+++ backend/ArtWall/api/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/', null=True,blank=True)
     price = models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True)
     user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
-    # category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
     # category = models.ForeignKey(Category, on_delete=models.CASCADE)
     category = models.ManyToManyField(Category, through='ProductCategory')
     def __str__(self):
