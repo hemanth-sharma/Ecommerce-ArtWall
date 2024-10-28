443086540726bc014c0e3d272c442ad64037f556
Hemant Sharma <hemantsharmafli7@gmail.com>
2024-10-28 13:07:14 +0530

    typo error

--- backend/ArtWall/ArtWall/settings.py
+++ backend/ArtWall/ArtWall/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessagfeMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
