999d6496fb966a1b66ce913f768026a8a78a697f
Hemant Sharma <hemantsharmafli7@gmail.com>
2024-11-01 20:17:09 +0530

    Setting up ngnix and docker

--- backend/ArtWall/ArtWall/settings.py
+++ backend/ArtWall/ArtWall/settings.py
@@ -181,15 +181,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/app/static')
 
-MEDIA_URL='/images/'
+MEDIA_URL='/media/'
 
 STATICFILES_DIRS =[
     BASE_DIR / 'static'
 ]
 
-MEDIA_ROOT= 'static/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/app/static/media')
+# MEDIA_ROOT = 'static/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
