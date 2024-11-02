fd54723a4f7079e2c4844c23d6abd582abd51c69
Hemant Sharma <hemantsharmafli7@gmail.com>
2024-11-02 22:55:41 +0530

    backend update: changed management file get_data.py

--- backend/ArtWall/api/management/commands/get_data.py
+++ backend/ArtWall/api/management/commands/get_data.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
                     name=poster['name'],
                     defaults={
                         'description': poster['description'],
-                        'image': self.download_image(poster['image']),  
+                        'image': poster['image'], # self.download_image(poster['image']),  
                         'price': round(random.uniform(20, 99)) *100,
                         'rating': round(random.uniform(1, 5), 1),
                         'reviews_count': random.randint(0, 1000),
@@ -49,22 +49,17 @@ class Command(BaseCommand):
         try:
             response = requests.get(image_url)
             if response.status_code == 200:
-                # Define the directory
                 products_dir = os.path.join(settings.MEDIA_ROOT, 'products')
-                # Create the directory if it doesn't exist
                 os.makedirs(products_dir, exist_ok=True)
 
                 # Create a unique file name
                 original_filename = os.path.basename(image_url)
-                file_extension = os.path.splitext(original_filename)[1]  # Get the file extension
-                unique_filename = f"{uuid.uuid4()}{file_extension}"  # Generate a unique name
+                file_extension = os.path.splitext(original_filename)[1] 
+                unique_filename = f"{uuid.uuid4()}{file_extension}" 
                 file_path = os.path.join(products_dir, unique_filename)
 
-                # Write the image to the file
-                with open(file_path, 'wb') as img_file:
-                    img_file.write(response.content)
-
-                return f"products/{unique_filename}"  
+                return image_url
+              
         except Exception as e:
             self.stdout.write(self.style.ERROR(f"Error downloading image: {e}"))
         return None
